ef473e646159a8154116b6ba7334aefb63570686
pradiptisingh19 <pradipti.singh8@gmail.com>
2024-06-26 16:57:12 +0530

    importing requires libraries and defining constants

---
+++ pendulum.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+g = 9.81  
+length = 1.0  
+theta0 = np.pi / 4.0  
+omega0 = 0.0  
+time_step = 0.05  
+total_time = 10.0 
